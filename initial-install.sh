74b62ca802bece1861f4abe0c99cd22fb60eca8c
Todd Costella <toddcostella@gmail.com>
2023-12-16 23:29:44 -0800

    Add libfuse2 package to vm.yml and rearrange commands in initial-install.sh
    In vm.yml, the libfuse2 package was added to the list of packages. In initial-install.sh, the order of the commands was rearranged to ensure that the necessary permissions are granted before certain actions are performed. Furthermore, 'sudo' was added to the removal command for the jetbrains toolbox file to avoid possible permission issues.

--- initial-install.sh
+++ initial-install.sh
@@ -6,12 +6,12 @@ cd ~/proj || exit
 git clone https://github.com/ToddCostella/ansible.git
 cd ansible || exit
 sudo apt install ansible -y
-sudo ansible-playbook vm.yml --ask-become-pass
 sudo cp offline-installers/jetbrains-toolbox-2.1.2.18853.tar.gz /opt
 cd /opt || exit
 sudo tar -xvzf jetbrains-toolbox-2.1.2.18853.tar.gz
 sudo mv jetbrains-toolbox-2.1.2.18853 jetbrains
-rm jetbrains-toolbox-2.1.2.18853.tar.gz
+sudo rm jetbrains-toolbox-2.1.2.18853.tar.gz -f
 jetbrains/jetbrains-toolbox
+sudo ansible-playbook vm.yml --ask-become-pass
 curl -sSL https://install.python-poetry.org | python3 -
 
