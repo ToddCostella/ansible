1846cae7a5ae34d0aac21051d945378df7b5b612
Todd Costella <toddcostella@gmail.com>
2023-12-16 22:06:07 -0800

    Add git installation and repository cloning to initial setup
    The script initial-install.sh now also includes steps for installing git and cloning a specific Ansible repository from GitHub. This is accomplished by creating a new directory for projects and proceeding with the cloning within it.

--- initial-install.sh
+++ initial-install.sh
@@ -1,9 +1,13 @@
 sudo apt update
 sudo apt upgrade -y
+sudo apt install git 
+mkdir ~/proj
+cd ~/proj || exit
+git clone https://github.com/ToddCostella/ansible.git
 sudo apt install ansible -y
 sudo ansible-playbook vm.yml --ask-become-pass
 cp offline-installers/jetbrains-toolbox-2.1.2.18853.tar.gz /opt
-cd /opt
+cd /opt || exit
 sudo tar -xvzf jetbrains-toolbox-2.1.2.18853.tar.gz
 sudo mv jetbrains-toolbox-2.1.2.18853 jetbrains
 rm jetbrains-toolbox-2.1.2.18853.tar.gz
