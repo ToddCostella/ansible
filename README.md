eae4cd1fd3ffef2b0f1110293ba45039b3396099
Todd Costella <ToddCostella@gmail.com>
2023-12-18 15:42:45 -0800

    Refactor ansible setup and add new setup script
    This commit removes various old ansible files and replaces them with a new `vm.yml` file which better encapsulates the required setup. It also adds an initial install script `initial-install.sh` that includes commands for an updated Ansible install procedure. Finally, it introduces a .gitignore file and updates the README to reflect these changes.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # ansible
 
-
-
 ```
-ansible-run.sh
-ansible-playbook local.yml -K -b
-chsh -s $(which zsh)
+sudo ./initial-install.sh
 ```
---
+++ initial-install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install ansible -y
+sudo ansible-playbook vm.yml --ask-become-pass
+cp offline-installers/jetbrains-toolbox-2.1.2.18853.tar.gz /opt
+cd /opt
+sudo tar -xvzf jetbrains-toolbox-2.1.2.18853.tar.gz
+sudo mv jetbrains-toolbox-2.1.2.18853 jetbrains
+rm jetbrains-toolbox-2.1.2.18853.tar.gz
+jetbrains/jetbrains-toolbox
+curl -sSL https://install.python-poetry.org | python3 -
+
